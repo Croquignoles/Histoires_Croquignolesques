69a9f1af6032c6cb493a28e722106c84ccb1723e
Croquignoles <101559016+Croquignoles@users.noreply.github.com>
2022-04-15 14:23:47 +0200

    Update index.php
    hahahaha

--- index.php
+++ index.php
@@ -30,6 +30,7 @@ else
 
 
 </nav>
+<?= hahahahaha ?>
 <?php for ($i=1; $i <= $nbhistoire; $i++) { 
     $maRequete = "SELECT * FROM histoires WHERE id_histoire = $i";
     $response = $BDD->query($maRequete);
