de42d887d279335a16377de07f9974506ef7fb74
lololezigoto <lhubacher@ensc.fr>
2022-04-15 16:16:21 +0200

    swaaaaag

--- includes/navbar_connected.php
+++ includes/navbar_connected.php
@@ -1,3 +1,9 @@
+<?php if(!isset($_SESSION)){
+    session_start();
+}
+
+?>
+
 <div class="container">
         
         <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
@@ -8,16 +14,23 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="index.php"><span class="glyphicon glyphicon-film"></span> MyMovies</a>
+                    <a class="navbar-brand" href="index.php"><span class="glyphicon glyphicon-film"></span> Un site internet quelconque</a>
                 </div>
                 <div class="collapse navbar-collapse" id="navbar-collapse-target">
+                    <?php if($_SESSION['isAdmin']==1){?>
+
+                    
                                     <ul class="nav navbar-nav">
-                            <li><a href="movie_add.php">Ajouter un film</a></li>
+                            <li><a href="movie_add.php">Créer une nouvelle histoire</a></li>
                         </ul>
+                        <ul class="nav navbar-nav">
+                            <li><a href="movie_add.php">Gérer les histoires existantes</a></li>
+                        </ul>
+                    <?php } ?>
                                 <ul class="nav navbar-nav navbar-right">
                                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                    <span class="glyphicon glyphicon-user"></span> Bienvenue, paul <b class="caret"></b>
+                                    <span class="glyphicon glyphicon-user"></span> Bienvenue, <?= $_SESSION['user']?> <b class="caret"></b>
                                 </a>
                                 <ul class="dropdown-menu">
                                     <li><a href="logout.php">Se déconnecter</a></li>
--- traitelogin.php
+++ traitelogin.php
@@ -8,20 +8,35 @@ $mdp = $_POST['password'];
 $isConncted = false;
 
 
-// Dans la requête, on remplace les valeurs issues de variables par des ?
-$maRequete = "SELECT * FROM users";
+/*// Dans la requête, on remplace les valeurs issues de variables par des ?
+$maRequete = "SELECT * FROM users WHERE (id_user=:login AND mdp_user=:mdp)";
 // On exécute la requête en lui fournissant les variables à utiliser dans l’ordre
-$response = $BDD->query($maRequete);
-while($ligne = $response->fetch()) {
-    if ($ligne["id_user"]==$login && $ligne["mdp_user"]==$mdp){
-        echo "Bonjour " . $login . "!";        
-        $_SESSION['user'] = $login;
+$response = $BDD->prepare($maRequete);
+$response = $BDD->execute($maRequete);*/
+
+$requete = "SELECT * FROM users WHERE id_user=? AND mdp_user=?";
+$response = $BDD->prepare($requete);
+$response->execute(array($login, $mdp));
+$ligne = $response->fetch();
+$nb = $response->rowCount(); 
+$isAdmin = $ligne['admin_user'];
+
+if ($nb ==0){                
+    echo "Utilisateur non reconnu ! "; 
+}else if ($nb==1){        
+     echo "Bonjour " . $login . "!";        
+        $_SESSION['user'] = $login;        
         header("Location: index.php");
-    } else {
-                echo "Utilisateur non reconnu ! "; 
-    }
+        if($isAdmin==0){
+            $_SESSION['isAdmin'] = 0;
+        } else {
+            $_SESSION['isAdmin'] = 1;        
+
 }
 
+} else {
+    echo "Chelou ta bdd mon reuf";
+}
 
 
 ?> 
