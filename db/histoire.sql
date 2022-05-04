8f5fca0f0064bf6e9423c34aac64b0443dbea5d3
lololezigoto <lhubacher@ensc.fr>
2022-05-04 11:09:44 +0200

    firstpage + page

--- db/histoire.sql
+++ db/histoire.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : ven. 22 avr. 2022 à 11:47
+-- Généré le : mer. 04 mai 2022 à 10:13
 -- Version du serveur : 10.4.19-MariaDB
 -- Version de PHP : 7.4.20
 
@@ -41,7 +41,8 @@ CREATE TABLE `histoires` (
 INSERT INTO `histoires` (`id_histoire`, `nom_histoire`, `description_histoire`, `image_histoire`) VALUES
 (1, 'Désolé pour hier soir', 'Tu es bourré lol  cest tout', 'defonce.jpg'),
 (2, 'Aventures en terre inconnue', 'Singeries et danger                                      ', 'foret.jpg'),
-(3, 'Gloubib', 'Blob', '');
+(3, 'Gloubib', 'Blob', ''),
+(4, 'atchoum', '                        sniff sniff              ', '');
 
 -- --------------------------------------------------------
 
@@ -53,32 +54,33 @@ CREATE TABLE `pages` (
   `id_pages` int(11) NOT NULL,
   `text_page` text DEFAULT NULL,
   `id_histoire` int(11) DEFAULT NULL,
-  `id_page_depart` int(11) DEFAULT NULL
+  `id_page_depart` int(11) DEFAULT NULL,
+  `desc_courte` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `pages`
 --
 
-INSERT INTO `pages` (`id_pages`, `text_page`, `id_histoire`, `id_page_depart`) VALUES
-(1, 'Mal à la tete plus réveil compliqué dans un trampoline. j\'entends un chien aboyer, qu\'est-ce que je fais : ', 1, NULL),
-(2, 'je décide d\'aboyer aussi et  rapidement, les pas assassins ornés de ballerines font trembler le sol, se rapprochent fatalement de moi. Mon rythme cardiaque augmente, je sens mon poul se mélengeant à mon souffle. Le gout du sang remplie ma bouche. Quant à elle, après quelques lourdes secondes, elle se met finalement à parler : \r\n\"Oh je vois que vous utilisez le trampoline de mon fils, on a décidé de le garder avec Marc depuis que ... (elle se mit a sangloter) que.. François a décidé de partir au Mali en mission humanitaire\".\r\n', 1, 1),
-(3, 'Je sors une friandise Zooplus de votre poche pour tenter de l\'amadouer', 1, 1),
-(4, 'Je sors tant bien que mal du trampoline et cours dans le champ situé derrrière vous\r\n', 1, 1),
-(5, 'J\'acquiesce sans trop comprendre mais finit par lui demander si elle sait comment je suis arrivé là, vous faites la conversation et arrivez dans la salle de bain, que regardez vous en premier							', 1, 2),
-(6, 'Il est mort ? Elle s\'énerve que vous ayez pu une seule seconde penser que son protégé est mort -pdv \r\nElle s\'excuse de sa réaction précipitée mais mal a l\'aise, vous demande de partir. Vous déambulez alors dans la ville à la recherche de réponses. Vous arrivez au centre ville, où allez vous? 	', 1, 2),
-(8, 'vos yeux : vos pupilles sont étrangement dilatées. Aussi vous vous demandez ce que vous avez fait hier soir pour atterir ici, vous dégainez votre téléphone pour appeler quelqu\'un qui vous aiderait	', 1, 5),
-(9, 'Vous composez le numéro de votre soeur', 1, 8),
-(10, 'Vous composez le numéro de Théotime', 1, 8),
-(11, 'votre corps d\'athlète : vous soulevez votre t shirt et la réalité vous rattrape, vous êtes un joueur de lol ne l\'oubliez pas		', 1, 5),
-(12, 'vos vetements : la taille et la couleur de vos vetement ne correspond pas à ce que vous avez l\'habtiude de porter, d\'autant plus que votre pantalon n\'est en fait rien d\'autre qu\'un bermuda. Horrifié vous plongez dans vos pensez pour tenter de vous rappeler votre soirée d\'hier. 		', 1, 5),
-(13, 'A la boulangerie', 1, 6),
-(14, 'Au kiosque à journaux', 1, 6),
-(15, 'sur un banc dans le parc pour enfant. Une jeune maman s\'assied à côté de vous et commence à vous parler de ses nuits horrible et de la complexité du changement de couche. -pdv', 1, 6),
-(16, 'En quelle année on est ? bein en 2022 guignol, elle commence a etre suspicieuse de votre etat et décide de vous expulser de chez elle car elle ne veut pas de souci, vous déambulez alors dans la ville de Tourcoing à la reherche de réponses et finissez par arriver au centre ville. Où allez vous ?		', 1, 2),
-(17, 'A la boulangerie', 1, 16),
-(18, 'Au kiosque à journaux', 1, 16),
-(19, 'sur un banc dans le parc pour enfant. Une jeune maman s\'assied à côté de vous et commence à vous parler de ses nuits horrible et de la complexité du changement de couche. -pdv', 1, 16);
+INSERT INTO `pages` (`id_pages`, `text_page`, `id_histoire`, `id_page_depart`, `desc_courte`) VALUES
+(1, 'Mal à la tete plus réveil compliqué dans un trampoline. j\'entends un chien aboyer, qu\'est-ce que je fais : ', 1, NULL, NULL),
+(2, 'Je décide d’aboyer aussi, et rapidement, des pas assassins ornés d’affreuses ballerines font trembler le sol, se rapprochant fatalement de moi. Mon rythme cardiaque augmente, j’entends mon cœur battre au niveau de mes tempes, mon pouls se mélange à mon souffle. Le goût métallique du sang rempli ma bouche. Quant à la supposée meurtrière, après de lourdes secondes, elle se met finalement à parler : « Oh je vois que vous utilisez le trampoline de mon fils, on a décidé de le garder avec Marc depuis que… (elle se mit a sangloter) que… François a décidé de partir au Mali en mission humanitaire ». \r\n\r\n', 1, 1, 'Je décide d\'aboyer aussi'),
+(3, 'Je sors une friandise Zooplus de votre poche pour tenter de l\'amadouer', 1, 1, 'Je sors une friandise Zooplus de votre poche pour tenter de l\'amadouer'),
+(4, 'Je sors tant bien que mal du trampoline et cours dans le champ situé derrrière moi.\r\n', 1, 1, 'Je sors tant bien que mal du trampoline et cours dans le champ situé derrrière moi.'),
+(5, 'J\'acquiesce sans trop comprendre mais finit par lui demander si elle sait comment je suis arrivé là, vous faites la conversation et arrivez dans la salle de bain, que regardez vous en premier							', 1, 2, 'J\'acquiesce sans trop comprendre.'),
+(6, 'Il est mort ? Elle s\'énerve que vous ayez pu une seule seconde penser que son protégé est mort -pdv \r\nElle s\'excuse de sa réaction précipitée mais mal a l\'aise, vous demande de partir. Vous déambulez alors dans la ville à la recherche de réponses. Vous arrivez au centre ville, où allez vous? 	', 1, 2, 'Il est mort ?'),
+(8, 'vos yeux : vos pupilles sont étrangement dilatées. Aussi vous vous demandez ce que vous avez fait hier soir pour atterir ici, vous dégainez votre téléphone pour appeler quelqu\'un qui vous aiderait	', 1, 5, 'Mes yeux'),
+(9, 'Vous composez le numéro de votre soeur', 1, 8, 'Je compose le numéro de ma soeur'),
+(10, 'Vous composez le numéro de Théotime', 1, 8, 'Je compose le numéro de Théotime'),
+(11, 'votre corps d\'athlète : vous soulevez votre t shirt et la réalité vous rattrape, vous êtes un joueur de lol ne l\'oubliez pas		', 1, 5, 'Mon corps d\'athlète'),
+(12, 'vos vetements : la taille et la couleur de vos vetement ne correspond pas à ce que vous avez l\'habtiude de porter, d\'autant plus que votre pantalon n\'est en fait rien d\'autre qu\'un bermuda. Horrifié vous plongez dans vos pensez pour tenter de vous rappeler votre soirée d\'hier. 		', 1, 5, 'Mes vêtements'),
+(13, 'A la boulangerie', 1, 6, 'A la boulangerie'),
+(14, 'Au kiosque à journaux', 1, 6, 'Au kiosque a journaux'),
+(15, 'sur un banc dans le parc pour enfant. Une jeune maman s\'assied à côté de vous et commence à vous parler de ses nuits horrible et de la complexité du changement de couche. -pdv', 1, 6, 'Sur un banc dans le parc pour enfant'),
+(16, 'En quelle année on est ? bein en 2022 guignol, elle commence a etre suspicieuse de votre etat et décide de vous expulser de chez elle car elle ne veut pas de souci, vous déambulez alors dans la ville de Tourcoing à la reherche de réponses et finissez par arriver au centre ville. Où allez vous ?		', 1, 2, 'En quelle année on est ?'),
+(17, 'A la boulangerie', 1, 16, 'A la boulangerie'),
+(18, 'Au kiosque à journaux', 1, 16, 'Au kiosque a journaux'),
+(19, 'sur un banc dans le parc pour enfant. Une jeune maman s\'assied à côté de vous et commence à vous parler de ses nuits horrible et de la complexité du changement de couche. -pdv', 1, 16, 'Sur un banc dans le parc pour enfant');
 
 -- --------------------------------------------------------
 
@@ -133,7 +135,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `histoires`
 --
 ALTER TABLE `histoires`
-  MODIFY `id_histoire` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_histoire` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT pour la table `pages`
--- page.php
+++ page.php
@@ -1,33 +1,22 @@
 <?php  
 session_start();
-$idpage = $_GET["idpagechoix"];
+$idpage = $_GET["idpage"];
+echo $idpage;
 $titrehistoire = $_GET["story"];
+$idhistoire = $_GET["idstory"];
 ?>
 
 <!doctype html>
 <html> 
 
 <?php include("includes/connect.php"); 
-$id = $_GET["id"];
-$maRequete1 = "SELECT * FROM histoires WHERE id_histoire=$id";
-$rep = $BDD->query($maRequete1);
-$nbhistoire = $rep->rowCount(); 
-    $ligne = $rep->fetch();
-    $id = $ligne["id_histoire"];
-    $title = $ligne["nom_histoire"];
-    $des_courte = $ligne["description_histoire"];
-    $image = $ligne["image_histoire"];
 
-$maRequete = "SELECT * FROM pages WHERE id_histoire = $id AND id_page_depart = $idpage";
- 
-    $response = $BDD->query($maRequete);
-    $ligne = $response->fetch();
-    $idpage = $ligne["id_pages"];
-    $texte = $ligne["text_page"];
-    $idhistoire = $ligne["id_histoire"];
-    $idpadedepart = $ligne["id_page_depart"];
-?>
-
+$maRequete = "SELECT * FROM pages WHERE id_histoire = $idhistoire AND id_pages = $idpage";
+                $response = $BDD->query($maRequete);
+                $ligne = $response->fetch();
+                    $textechoix1 = $ligne["text_page"];
+                    $descourte1 = $ligne["desc_courte"];
+                    ?>
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -46,23 +35,36 @@ else
     include("includes/navbar.php"); 
 ?>
 
-</nav>
-
-        <div class="jumbotron">
+<div class="jumbotron">
             <div class="row">
-                <div class="col-md-5 col-sm-7">
-                    <img class="img-responsive movieImage" src="images/<?= $image ?>" title=<?= $title?> />
+                <div class="col-1">
+                    <p><?= $textechoix1?></p>
                 </div>
-                <div class="col-md-7 col-sm-5">
-                    <p><small><?= $des_courte?></small></p>
-                </div>
-                <div class="col-md-7 col-sm-5">
-                <a href="page.php?story=<?=$title?>&id=<?=$id?>" class="btn btn-info" role="button"> Let's go !</a>
-                </div>
+                <?php
+                $maRequete2 = "SELECT * FROM pages WHERE id_histoire = $idhistoire AND id_page_depart = $idpage";
+                $response2 = $BDD->query($maRequete2);
+                $nbchoix = $response2->rowCount();
+                foreach ($response2 as $ligne2)
+                {
+                    $idpagechoix = $ligne2["id_pages"];
+                    $textechoix = $ligne2["text_page"];
+                    $descourte = $ligne2["desc_courte"];
+                    ?>
+
+                    <div class="col-sm-<?=$nbchoix?>">
+                    <p><small><?= $descourte?></small></p>
+                    <a href="page.php?story=<?=$titrehistoire?>&idstory=<?=$idhistoire?>&idpage=<?=$idpagechoix?>" class="btn btn-info" role="button"> J'y vais !</a>
+                    </div>
+                <?php
+                }
+                
+                ?>
+                
             </div>
         </div>
 
-        
+
+
     <footer class="footer">
     Construit avec swag par lololezigoto, élève de l'<a href="https://www.ensc.fr">ENSC</a>.
 </footer></div>
