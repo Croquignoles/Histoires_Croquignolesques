f0d3d4cc9c87545c58aeda2d6224a351ea60326a
lololezigoto <lhubacher@ensc.fr>
2022-05-04 11:45:18 +0200

    +1 quand on joue

--- includes/connect.php
+++ includes/connect.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-$BDD = new PDO( "mysql:host=localhost;dbname=histoire;charset=utf8",
+$BDD = new PDO( "mysql:host=localhost;dbname=histoires;charset=utf8",
 "user","secret", array(PDO::ATTR_ERRMODE
 =>PDO::ERRMODE_EXCEPTION));
 }
